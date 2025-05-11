79c44dafe217f13b5f1b6186c72b998d3ff71845
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2025-05-11 18:48:09 -0400

    changed about us
    better information on how to use readawiki

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -11,7 +11,8 @@ function ModalPopup({ isSidebarOpen, open, closeModal }) {
           OpenFiche was a project built at SpartaHack X. We reflected on how Google Search's current algorithm has been declining in quality, leading it to produce less relevant
           results, as the <a target='_blank' href="https://downloads.webis.de/publications/papers/bevendorff_2024a.pdf" rel="noopener noreferrer">SEO is slammed with tags</a>. We remember how effective Google Search's algorithm used to be before 
           SEO tagging became commonplace, and in hopes to return to a search engine of this quality, we reimplemented Google's Pagerank indexing Wikipedia (CS-related articles), CNN, and NPR using PageRank + 
-          keyword scoring. This allowed us to rank pages based on relevance, not SEO tricks, and we complimented this tool with a minimalist web browser that helps academics zone in on their research.
+          keyword scoring. This allowed us to rank pages based on relevance, not SEO tricks, and we complimented this tool with a minimalist web browser that helps academics zone in on their research.<br></br><br></br>
+          We indexed about 300,000 pages, all relating to US politics and Computer Science. Searches about these topics will yield better results.
         </p>
       </div>
     </Popup>
